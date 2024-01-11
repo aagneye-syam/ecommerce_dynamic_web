cc93a98bb8e51edcd0622e58caf64a34230f7fb0
aagneye-syam <saagneye2003@gmail.com>
2024-01-11 20:48:17 +0530

    added cards in index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let products=[
+    {
+      name:"Levis Jacket",
+      category:"Jacket",
+      descrption:"Levi’s® Made & Crafted SS23 Collection Delivers a Contemporary Outlook",
+      image:""
+    },
+    {
+      name:"Jcaket",
+      category:"Jacket",
+      descrption:"LEVI’S 501 Jeans Collection",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPIv53II1zhL5InjgNuTxHhjC9hurd0Ca1lZGz-1zIbU8_mjRznTr-0BPkTatNFF6IKYw&usqp=CAU"
+    },
+    {
+      name:"Blue Pants",
+      category:"Pants",
+      descrption:"Crafted SS23 Collection ",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVyhi4e2_7ixiiEao8M9HXoBrQv9KDCjGQsg&usqp=CAU",
+    },
+    {
+      name:"Dark blue Tone",
+      category:"Jeans",
+      descrption:"Off 68% for the fall",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF97JOLjcmTIDOJM3aUnihD2ww_Mg6CsBXdjOtadiw3djw8xElC6rALs78CWx2rRX-AaA&usqp=CAU",
+    }
+  ]
   res.render('index', { title: 'Express' });
 });
 
