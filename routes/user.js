31d0164304e4e847854df8664cc1e4f1b418482e
aagneye-syam <saagneye2003@gmail.com>
2024-01-12 21:07:00 +0530

    teable created on admin view-products

--- routes/user.js
+++ routes/user.js
@@ -7,29 +7,29 @@ router.get('/', function(req, res, next) {
     {
       name:"Levis Jeans",
       category:"Jeans",
-      descrption:"Levis 501 jeans ",
+      description:"Levis 501 jeans",
       image:"https://bestmediainfo.com/uploads/2023/03/1678162677.Levis-501R-featuring-Deepika-Padukone.jpg",
     },
     {
       name:"Jacket",
       category:"Jacket",
-      descrption:"LEVIS 501 Jeans Collection",
+      description:"LEVIS 501 Jeans Collection",
       image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPIv53II1zhL5InjgNuTxHhjC9hurd0Ca1lZGz-1zIbU8_mjRznTr-0BPkTatNFF6IKYw&usqp=CAU"
     },
     {
       name:"Blue Pants",
       category:"Pants",
-      descrption:"Crafted SS23 Collection ",
+      description:"Crafted SS23 Collection",
       image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVyhi4e2_7ixiiEao8M9HXoBrQv9KDCjGQsg&usqp=CAU",
     },
     {
       name:"Dark blue Tone",
       category:"Jeans",
-      descrption:"Off 68% for the fall",
+      description:"Off 68% for the fall",
       image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF97JOLjcmTIDOJM3aUnihD2ww_Mg6CsBXdjOtadiw3djw8xElC6rALs78CWx2rRX-AaA&usqp=CAU",
     }
   ]
-  res.render('index', {products, user:true});
+  res.render('index', {products,user:true});
 });
 
 module.exports = router;
